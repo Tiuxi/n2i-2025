dd53141cd1141d04c8df7954d6f15d267dd98fae
EHRHARD ALEXANDRE <alexandre.ehrhard@etu.unistra.fr>
2025-12-05 08:01:33 +0100

    Nom des pages

--- email.php
+++ email.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Force a vous :D</title>
+    <title>NIRD - Formulaire</title>
 
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="assets/styles/form.css">
